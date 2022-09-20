cdb0d47bb90b489d86a6599846bd33b84cc14543
Jose Jimenez <113552145+jjosxe@users.noreply.github.com>
2022-09-20 19:30:10 +0000

    Booleans Practice

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
-# my_bool = 5 > 4
-#print(my_bool)
+my_bool = 5 > 4
+print(my_bool)
 
 # > greater
 # < less
@@ -86,28 +86,32 @@ raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
 # != different or not equal to
 
 # you can also construct booleans to see if values ar in a variable or not found
-# my_ bool = 5 in my_list
-# my_bool = 5 not in my_list
+my_list = [1,2,3,4,5,6,7]
+my_bool = 5 in my_list #true
+my_bool = 5 not in my_list #false
 
 # so we can see if we can make logical decisions if something is true or not
-# var1 = True
-# var2 = False
-# print(type(var1))
-# print(var1)
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
 
-# list = [1,2,3,4,5,6]
-# control = 5 in list
-# print(type(control))
-# print(control)
+list = [1,2,3,4,5,6]
+control = 5 in list
+print(type(control))
+print(control)
 
 # Booleans Practice
 
 # Make a comparison that returns a boolean and store the result (True/False) in a variable called test
-
+my_bool = 6<3
+print(my_bool)
 # Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
-
+my_bool = 17834/34 > 87*56
+print(my_bool)
 # Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print
-
+import math
+print(math.sqrt(25) == 5)
 
 
 #############################################next week ################################################
