f35128a00e53c861b14d886a1a13f9862477a2df
Jose Jimenez <113552145+jjosxe@users.noreply.github.com>
2022-09-20 19:08:57 +0000

    Counted the number of something in a tuple, Converted a tuple into a list and also extracted elements

--- main.py
+++ main.py
@@ -25,21 +25,23 @@ print(my_tuple[2][1])
 
 # place these numbers in separate variables from the tuple below
 t = (1, 2, 3)
-
+x,y,z = t
+print(x,y,z)
 #get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
-
+print(len(my_tuple))
 # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
-
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
-
+print(my_tuple.count(2))
 # Convert the following tuple to a list, and store it in a variable called my_list.
-
+my_list = list(my_tuple)
+print(my_list)
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
 
 #Extract the elements of the following tuple into four variables: a, b, c, d
 
 my_tuple = (1, 2, 3, 4)
-
+a,b,c,d = my_tuple
+print(a,b,c,d)
 
 #################################################sets######################################################
 
